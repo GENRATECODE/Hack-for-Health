36958dbfb5c93eec9927c56bf2f1843c2f0b976e
Abhay Swarnakar <68654016+GENRATECODE@users.noreply.github.com>
2024-01-10 22:35:36 +0530

    add model

--- Health.py
+++ Health.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 class model_input(BaseModel):
+    # heart Diease 
     age:int
     sex: int
     cp: int
@@ -41,9 +42,30 @@ class model_input(BaseModel):
     slope:int
     ca:int
     thal:int
-# for parkison 
-
-# for Diabeties 
+    # for parkison 
+    MDVP_Fo_Hz:float
+    MDVP_Fhi_Hz:float
+    MDVP_Flo_Hz:float
+    MDVP_jitter_percentage:float
+    MDVP_Jitter_Abs:float
+    MDVP_RAP:float
+    MDVP_PPQ:float
+    Jitter_DDP:float
+    MDVP_Shimmer:float
+    MDVP_Shimmer_dB:float
+    Shimmer_APQ3:float
+    Shimmer_APQ5:float
+    MDVP_APQ:float
+    Shimmer_DDA:float
+    NHR:float
+    HNR:float
+    RPDE:float
+    DFA:float
+    spread1:float
+    spread2:float
+    D2:float
+    PPE:float
+    # for Diabeties 
     Pregnancies :int
     Glucose  :int       
     BloodPressure :int
@@ -83,8 +105,16 @@ def result_Diabeties(Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI
     return json.dumps("check_2")
 
 #   Parkison function 
-def result_parkinson():
-    pass
+def result_parkinson(MDVP_Fo_Hz,MDVP_Fhi_Hz,MDVP_Flo_Hz,MDVP_jitter_percentage,MDVP_Jitter_Abs,MDVP_RAP,MDVP_PPQ,Jitter_DDP,MDVP_Shimmer,MDVP_Shimmer_dB,Shimmer_APQ3,Shimmer_APQ5,MDVP_APQ,Shimmer_DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE):
+    column =['MDVP_Fo_Hz','MDVP_Fhi_Hz','MDVP_Flo_Hz','MDVP_jitter_percentage','MDVP_Jitter_Abs','MDVP_RAP','MDVP_PPQ','Jitter_DDP','MDVP_Shimmer','MDVP_Shimmer_dB','Shimmer_APQ3','Shimmer_APQ5','MDVP_APQ','Shimmer_DDA','NHR','HNR','RPDE','DFA','spread1','spread2','D2','PPE']
+    data=[MDVP_Fo_Hz,MDVP_Fhi_Hz,MDVP_Flo_Hz,MDVP_jitter_percentage,MDVP_Jitter_Abs,MDVP_RAP,MDVP_PPQ,Jitter_DDP,MDVP_Shimmer,MDVP_Shimmer_dB,Shimmer_APQ3,Shimmer_APQ5,MDVP_APQ,Shimmer_DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]
+    print(data)
+    x=pd.DataFrame(data=data,columns=column)
+    print(x)
+    filename="models/Parkison.pickle"
+    # loaded_model=pi.load(open(filename,'rb'))
+    # return json.dump(output_label(loaded_model.predict(),"Parkison "))
+    return json.dumps("check_3")
 # Setting up the home route
 @app.get("/")
 def read_root():
@@ -108,8 +138,31 @@ async def prediction_heart(input_parameters: model_input):
     thal=input_parameters.thal
     return result_heart(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
 
-# @app.post("/parkinson")
-# async def prediction_parkinson(input_parameters:model_input):
+@app.post("/parkinson")
+async def prediction_parkinson(input_parameters:model_input):
+    MDVP_Fo_Hz=input_parameters.MDVP_Fo_Hz
+    MDVP_Fhi_Hz=input_parameters.MDVP_Fhi_Hz
+    MDVP_Flo_Hz=input_parameters.MDVP_Flo_Hz
+    MDVP_jitter_percentage=input_parameters.MDVP_jitter_percentage
+    MDVP_Jitter_Abs=input_parameters.MDVP_Jitter_Abs
+    MDVP_RAP=input_parameters.MDVP_RAP
+    MDVP_PPQ=input_parameters.MDVP_PPQ
+    Jitter_DDP=input_parameters.Jitter_DDP
+    MDVP_Shimmer=input_parameters.MDVP_Shimmer
+    MDVP_Shimmer_dB=input_parameters.MDVP_Shimmer_dB
+    Shimmer_APQ3=input_parameters.Shimmer_APQ3
+    Shimmer_APQ5=input_parameters.Shimmer_APQ5
+    MDVP_APQ=input_parameters.MDVP_APQ
+    Shimmer_DDA=input_parameters.Shimmer_DDA
+    NHR=input_parameters.NHR
+    HNR=input_parameters.HNR
+    RPDE=input_parameters.RPDE
+    DFA=input_parameters.DFA
+    spread1=input_parameters.spread1
+    spread2=input_parameters.spread2
+    D2=input_parameters.D2
+    PPE=input_parameters.PPE
+    return result_parkinson(MDVP_Fo_Hz,MDVP_Fhi_Hz,MDVP_Flo_Hz,MDVP_jitter_percentage,MDVP_Jitter_Abs,MDVP_RAP,MDVP_PPQ,Jitter_DDP,MDVP_Shimmer,MDVP_Shimmer_dB,Shimmer_APQ3,Shimmer_APQ5,MDVP_APQ,Shimmer_DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE)
 
 @app.post("/Diabeties")
 async def prediction_diabeties(input_parameters:model_input):
